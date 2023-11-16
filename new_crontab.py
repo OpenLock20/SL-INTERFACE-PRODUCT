b096595ab956616ee6f27004225f898bf7f6c173
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2023-11-16 11:37:30 -0600

    Se agrega nueva configuracion para crontab

---
+++ new_crontab.py
@@ -0,0 +1,36 @@
+import subprocess
+
+
+cronjob = f"""# Edit this file to introduce tasks to be run by cron.
+#
+# Each task to run has to be defined through a single line
+# indicating with different fields when the task will be run
+# and what command to run for the task
+# 
+# To define the time you can provide concrete values for
+# minute (m), hour (h), day of month (dom), month (mon),
+# and day of week (dow) or use '*' in these fields (for 'any').
+# 
+# Notice that tasks will be started based on the cron's system
+# daemon's notion of time and timezones.
+# 
+# Output of the crontab jobs (including errors) is sent through
+# email to the user the crontab file belongs to (unless redirected).
+# 
+# For example, you can run a backup of all your user accounts
+# at 5 a.m every week with:
+# 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
+# 
+# For more information see the manual pages of crontab(5) and cron(8)
+# 
+# m h  dom mon dow   command
+
+1 3 * * * python3 /var/www/html/admin/new_crontab.py
+* * * * * python3 /var/www/html/admin/DHCP_activation.py ; service pihole-FTL restart
+* * * * * python3 /var/www/html/admin/automatizacion.py
+0 3 * * * cd /var/www/html/admin ; git pull"""
+
+with open('/tmp/cronjob', 'w') as cronfile:
+    cronfile.write(cronjob + '\n')
+subprocess.call(['sudo', 'crontab', '/tmp/cronjob'])
+print("Tareas configuradas.")
