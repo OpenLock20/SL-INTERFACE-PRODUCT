c8fb65a52a739f060bd601951ef487bf91cb3085
Elisfer11 <elisfer11@outlook.com>
2023-11-21 09:58:35 -0600

    Se hacen ajustes

--- scripts/pi-hole/php/CORREO/crontab_correo.py
+++ scripts/pi-hole/php/CORREO/crontab_correo.py
@@ -5,6 +5,9 @@ archivo_conf = "configuracion_correo.txt"
 with open(archivo_conf, 'r') as archivo:
     conf_crontab = archivo.read().strip()
 
+# Comando para agregar la tarea al crontab
+cron_command = f'echo "{conf_crontab}" | crontab -'
+
 # Verificar si el crontab ya contiene las tareas
 try:
     crontab_content = subprocess.check_output(['crontab', '-l'], universal_newlines=True)
@@ -13,11 +16,10 @@ try:
     else:
         cronjob = f"""* * * * * python3 /var/www/html/admin/scripts/pi-hole/php/CORREO/crontab_correo.py
 {conf_crontab}"""
-        
-        with open('/tmp/cronjob', 'w') as cronfile:
-            cronfile.write(cronjob + '\n')
-        
-        subprocess.call(['crontab', '/tmp/cronjob'])
+
+        # Agregar el cronjob al crontab
+        subprocess.run(cron_command, shell=True, check=True)
+
         print("Tareas configuradas en el crontab.")
 except subprocess.CalledProcessError as e:
     print(f"Error al obtener el crontab: {e}")
