bb9b1bd00228bd8f7af623d60ea79487ccb375c9
Elisfer11 <elisfer11@outlook.com>
2023-11-22 15:50:07 -0600

    Ajustes

--- estadisticas/estadisticas.php
+++ estadisticas/estadisticas.php
@@ -4,40 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estadísticas de Dominios</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <h1>Estadísticas de Dominios</h1>
-    <p id="dominios"></p>
-    <p id="consultasBloqueadas"></p>
-    <p id="porcentajeBloqueo"></p>
-    <p id="consultasTotales"></p>
+    <div class="contenedor-estadisticas">
+        <h1>Estadísticas de Dominios</h1>
+        <div id="dominios" class="estadistica"></div>
+        <div id="consultasBloqueadas" class="estadistica"></div>
+        <div id="porcentajeBloqueo" class="estadistica"></div>
+        <div id="consultasTotales" class="estadistica"></div>
 
-    <script>
-        function actualizarContenido(archivo, elementoId) {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    document.getElementById(elementoId).innerText = archivo + ': ' + xhr.responseText;
-                }
-            };
-            xhr.open('GET', 'actualizar_stats.php?archivo=' + archivo, true);
-            xhr.send();
-        }
+        <script>
+            function actualizarContenido(archivo, elementoId, nombreLegible) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        document.getElementById(elementoId).innerHTML = '<span class="nombre-archivo">' + nombreLegible + '</span>: ' + xhr.responseText;
+                    }
+                };
+                xhr.open('GET', 'actualizar_stats.php?archivo=' + archivo, true);
+                xhr.send();
+            }
 
-        // Actualizar cada segundo
-        setInterval(function() {
-            actualizarContenido('dominios_en_listas.txt', 'dominios');
-            actualizarContenido('consultas_bloqueadas.txt', 'consultasBloqueadas');
-            actualizarContenido('porcentaje_bloqueo.txt', 'porcentajeBloqueo');
-            actualizarContenido('consultas_totales.txt', 'consultasTotales');
-        }, 1000);
+            // Actualizar cada segundo
+            setInterval(function() {
+                actualizarContenido('dominios_en_listas.txt', 'dominios', 'Dominios en Listas');
+                actualizarContenido('consultas_bloqueadas.txt', 'consultasBloqueadas', 'Consultas Bloqueadas');
+                actualizarContenido('porcentaje_bloqueo.txt', 'porcentajeBloqueo', 'Porcentaje de Bloqueo');
+                actualizarContenido('consultas_totales.txt', 'consultasTotales', 'Consultas Totales');
+            }, 1000);
 
-        // Actualizar por primera vez al cargar la página
-        actualizarContenido('dominios_en_listas.txt', 'dominios');
-        actualizarContenido('consultas_bloqueadas.txt', 'consultasBloqueadas');
-        actualizarContenido('porcentaje_bloqueo.txt', 'porcentajeBloqueo');
-        actualizarContenido('consultas_totales.txt', 'consultasTotales');
-    </script>
+            // Actualizar por primera vez al cargar la página
+            actualizarContenido('dominios_en_listas.txt', 'dominios', 'Dominios en Listas');
+            actualizarContenido('consultas_bloqueadas.txt', 'consultasBloqueadas', 'Consultas Bloqueadas');
+            actualizarContenido('porcentaje_bloqueo.txt', 'porcentajeBloqueo', 'Porcentaje de Bloqueo');
+            actualizarContenido('consultas_totales.txt', 'consultasTotales', 'Consultas Totales');
+        </script>
+    </div>
 </body>
 </html>
 
