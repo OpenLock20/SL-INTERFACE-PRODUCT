78ce0e3eaf9ad171676d30e271addaba3626e2a3
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2024-02-06 17:03:35 -0600

    Update stats.php

--- stats.php
+++ stats.php
@@ -10,6 +10,20 @@
 $indexpage = true;
 require 'scripts/pi-hole/php/header_authenticated2.php';
 require_once 'scripts/pi-hole/php/gravity.php';
+
+
+// Incluir lógica para manejar la solicitud POST
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['cambiarEstado'])) {
+    $statusFile = '/var/www/html/admin/scripts/pi-hole/php/control_parental/parental_control_status.txt';
+    $status = trim(file_get_contents($statusFile));
+    $newStatus = $status === "Habilitado" ? "Deshabilitado" : "Habilitado";
+    file_put_contents($statusFile, $newStatus);
+
+    // Opcionalmente, redirige de nuevo a la misma página para evitar reenvíos de formulario
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 ?>
 
 <head>
@@ -76,16 +90,24 @@ require_once 'scripts/pi-hole/php/gravity.php';
         </div>
     </div>
     <section>
+<?php
+$statusFile = '/var/www/html/admin/scripts/pi-hole/php/control_parental/parental_control_status.txt';
+
+$status = trim(file_get_contents($statusFile));
+
+// Determinar el texto y el color del botón basado en el estado
+$buttonText = $status === "Habilitado" ? "Deshabilitar" : "Habilitar";
+$buttonColor = $status === "Habilitado" ? "btn-red" : "btn-green";
+?>
 
     <div class="control-parental-container">
+        <form id="formControlParental" method="post" style="display:none;">
+            <input type="hidden" name="cambiarEstado" value="1">
+        </form>
         <h1 class="control-parental-title">Control Parental</h1>
-        <label class="switch">
-            <input type="checkbox" id="parentalControlSwitch">
-            <span class="slider round"></span>
-            <span class="switch-label off">Off</span>
-            <span class="switch-label on">ON</span>
-        </label>
+        <button id="parentalControlButton" class="btn <?php echo $buttonColor; ?>"><?php echo $buttonText; ?> Control Parental</button>
     </div>
+
 
     <div class="button-container text-center">
         <a href="index.php" class="btn btn-green">Ver más estadísticas</a>
@@ -99,6 +121,11 @@ require_once 'scripts/pi-hole/php/gravity.php';
             var ventanaEmergente = window.open('scripts/pi-hole/php/CORREO/registro_correo.php', 'RegistroCorreo', 'width=600, height=600, top=' + (screen.height/2 - 200) + ', left=' + (screen.width/2 - 300));
 
         }
+    </script>
+    <script>
+        document.getElementById('parentalControlButton').addEventListener('click', function() {
+            document.getElementById('formControlParental').submit();
+        });
     </script>
 
 
