34159f742638f377d5646b9ddec99df6af28da72
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2024-02-07 15:07:45 -0600

    Update domain_conf.py

--- domain_conf.py
+++ domain_conf.py
@@ -17,10 +17,10 @@ def actualizar_hosts(ip_nueva):
     encontrada_safelock = False  # Indicador de si encontramos la entrada safelock.ol
     # Espacios requeridos entre la IP y safelock.ol
     espacios = " " * 5
-    
+
     with open(archivo_hosts, 'r') as file:
         lineas = file.readlines()
-    
+
     with open(archivo_hosts, 'w') as file:
         for linea in lineas:
             if "localhost" in linea:
@@ -37,14 +37,19 @@ def actualizar_hosts(ip_nueva):
                     # Actualizar la línea para safelock.ol con 5 espacios
                     linea = ip_nueva + espacios + "safelock.ol\n"
             file.write(linea)
-        
+
         # Si no se encontró safelock.ol, agregarlo al final
         if not encontrada_safelock:
             necesita_actualizacion = True
             file.write(ip_nueva + espacios + "safelock.ol\n")
-    
+
     return necesita_actualizacion
 
 # Especifica la interfaz deseada, por ejemplo, "eth0"
 interfaz_deseada = "eth0"
 ip_actual = obtener_informacion_red(interfaz_deseada)
+
+if ip_actual and actualizar_hosts(ip_actual):
+    print("El archivo /etc/hosts ha sido actualizado con la nueva dirección IP.")
+else:
+    print("No se requiere actualización o la interfaz especificada no está disponible.")
