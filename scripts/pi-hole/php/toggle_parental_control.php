0442548ff0947407c578af69c9a34441eb1453de
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2024-02-07 14:34:40 -0600

    Delete scripts/pi-hole/php/toggle_parental_control.php

--- scripts/pi-hole/php/toggle_parental_control.php
+++
@@ -1,8 +0,0 @@
-<?php
-$action = $_GET['action'] ?? ''; // Recoger acción de la URL
-
-if ($action === 'enable' || $action === 'disable') {
-    $command = escapeshellcmd("/usr/bin/python3 /var/www/html/admin/scripts/pi-hole/php/control_parental/DisableAndEnable.py " . $action);
-    shell_exec($command);
-    header("Location: /admin/index.php"); // Redirige
-?>
