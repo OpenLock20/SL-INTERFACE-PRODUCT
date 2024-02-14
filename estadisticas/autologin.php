24873da986e6e22e37770395793a089abbe07916
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2024-02-14 08:53:38 -0600

    Create autologin.php

---
+++ estadisticas/autologin.php
@@ -0,0 +1,14 @@
+<?php
+session_start(); // Inicia la sesión PHP al principio del archivo
+require_once '../scripts/pi-hole/php/password.php'; // Asegúrate de que la ruta al archivo password.php sea correcta
+
+if (isset($_GET['password']) && $_GET['password'] === 'safelock') {
+    $_SESSION['auth'] = true;
+    $_SESSION['safelock_access'] = true; // Indica el acceso safelock
+    header('Location: ../stats.php');
+    exit;
+} else {
+    header('Location: ../login.php');
+    exit;
+}
+?>
